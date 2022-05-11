f8820b8bbf468cde0bd079eed9a2df7bead3951f
siriusjunior <topdownreading@gmail.com>
2022-05-11 16:56:19 +0900

    [Add]シードデータの反映,[Fix]ルーティングtypo

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,6 @@ class Food < ApplicationRecord
   belongs_to :restaurant
   belongs_to :order, optional: true
   has_one :line_food
+
+  validates :description, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       resources :restaurants do
         resources :foods, only: %i[index]
       end
-      resources :line_foods, %i[index create]
+      resources :line_foods, only: %i[index create]
       put 'line_foods/replace', to: 'line_foods#replace'
       resources :orders, only: %i[create]
     end
