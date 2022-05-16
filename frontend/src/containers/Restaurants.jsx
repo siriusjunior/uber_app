e267acc3b9528f5d12171b2bd6ccd9c98252f650
siriusjunior <topdownreading@gmail.com>
2022-05-16 17:55:04 +0900

    [Add]CORSを設定

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,6 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+
+//apis
+import { fetchRestaurants } from '../apis/restaurants';
 
 export const Restaurants = () => {
+
+  useEffect(() => {
+    fetchRestaurants()
+    .then((data)=>
+      console.log(data)
+    )
+  },[])
+  // コンポーネントのレンダリング時に一度だけ実行するので第二引数に空配列
+
   return (
     <Fragment>
       レストラン一覧
